e96c18869ecbdf9ad3d8df47a48abd9b60298e61
Abhinav Pillai <abhinavpillai92@gmail.com>
2025-03-31 23:54:42 +0530

    removed next auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@
 import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
